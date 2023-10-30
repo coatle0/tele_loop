323ae9de65ce25b48ceac8aeeb9c229ba240cb82
coatle0 <coatle0@gmail.com>
2023-10-30 15:06:18 +0900

    dbug

--- test_kiwoom.py
+++ test_kiwoom.py
@@ -143,7 +143,7 @@ def run_bot(queue_in,queue_out):
                         #print(msg_dic[cmd.split(' ')[1]])
                         #await client.edit_message(entity=my_bot_ch, message=msg_dic[cmd.split(' ')[1]],text=cmd.split('_')[1])
                         await client.delete_messages(entity=my_bot_ch, message_ids=msg_dic[cmd.split(' ')[1]])
-                        await client.send_message(my_bot_ch,text=cmd.split('_')[1])
+                        await client.send_message(my_bot_ch,cmd.split('_')[1])
 
                     else:
                         mes=await client.send_message(my_bot_ch,cmd.split('_')[1])
