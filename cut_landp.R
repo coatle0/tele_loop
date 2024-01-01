50963a7d845eecbb930d2e85232a9d5cec1f898c
coatle0 <coatle0@gmail.com>
2024-01-01 16:18:03 +0900

    add pf_us lst

--- cut_landp.R
+++ cut_landp.R
@@ -8,12 +8,12 @@ tmenv <- new.env()
 code<-code_get(fresh = TRUE)
 
 #get my pf list
-pf_fn <- 'pf_idx'
-pf_gs_lst <- read_rtgs_idx(pf_fn)
-pfwt_lst<-pf_gs_lst[[2]]
+pf_tbl <- read_asgs_sheet('portf_rt')
+nofkjm <- which(pf_tbl$jm=='US')-1
+nofusjm <- which(pf_tbl$jm=='Renesas')-1
 
-pfsmb_lst<-pf_gs_lst[[1]]
-pfsmb_ary<-unlist(pfsmb_lst)
+pfsmb_ary <- pf_tbl$jm[1:nofkjm]
+pf_us_lst <- pf_tbl$jm[(nofkjm+2):nofusjm]
 
 pf_smb_bb<-lapply(pfsmb_ary,function(x){tqk_code<-code[match(x,code$name),3]$code;
 
@@ -37,6 +37,52 @@ return(c(coredata(tail(temp_bbs$dn,n=1)$dn)*1.015,coredata(tail(temp_bbl$up,n=1)
 )
 
 names(pf_smb_bb)<-pfsmb_ary
+
+pf_us_bb<-lapply(t(pf_us_lst),function(x){print(x);
+
+temp<-get(getSymbols(x,from=start_date))[,1:5];
+    colnames(temp) <- c('open','high','low','close','volume');
+
+    temp_x<-temp;
+
+#bb long calculate
+    temp_bbl <- BBands(temp_x[,c("high","low","close")],n=bb_win_szl)
+    temp_atrl <- ATR(temp_x[,c("high","low","close")],n=bb_win_szl)
+
+#bb short calculate
+#bb long calculate
+    temp_bbs <- BBands(temp_x[,c("high","low","close")],n=bb_win_szs)
+    temp_atrs <- ATR(temp_x[,c("high","low","close")],n=bb_win_szs)
+
+    temp_bbl$close<-temp_x$close
+
+
+
+    temp_bbl<-tail(temp_bbl,n=120)
+    temp_atrl <- tail(temp_atrl,n=120)
+
+    temp_bbl<-temp_bbl[,-4]
+
+    temp_bbs<-tail(temp_bbs,n=120)
+    temp_atrs <- tail(temp_atrs,n=120)
+
+
+    temp_bbl$atrl <- temp_atrl$trueLow
+    temp_bbl$dn <- temp_bbs$dn
+    temp_bbl$nvol<-temp_x$nvol
+
+#colnames(temp_bbl)<-paste0(x,colnames(temp_bbl))
+    return(c(coredata(tail(temp_bbs$dn,n=1)$dn)*1.015,coredata(tail(temp_bbl$up,n=1)$up)*1.03,0))
+    #return(temp_bbl)
+}
+)
+names(pf_us_bb)<-t(pf_us_lst)
+
+pf_us_exit <-data.frame(t(floor(bind_rows(pf_us_bb)*100)/100))
+pf_smb_exit <-data.frame(t(floor(bind_rows(pf_smb_bb))))
+pf_losscut_df<-rbind(pf_smb_exit,NA,pf_us_exit)
+write_asgs_sheet(pf_losscut_df,'losscut','A1')
+
 
 while(TRUE) {
 
