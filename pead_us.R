b079b9dc1863f7082e658c7a4e35c0dafc5c4259
coatle0 <coatle0@gmail.com>
2024-02-10 22:32:32 +0900

    initial load

---
+++ pead_us.R
@@ -0,0 +1,74 @@
+setwd('~')
+tickerData <- new.env()
+
+bb_win_szl <- 20
+bb_win_szs <- 8
+
+idx_gs_lst <- read_gs_idx('pead.us')
+kweight_lst<-idx_gs_lst[[2]]
+ksmb_lst<-idx_gs_lst[[1]]
+
+df_tmp <- list()
+
+for (i in 1:length(ksmb_lst)){
+
+start_date<- as.character(kweight_lst[[i]]-days(32))
+ref_date <- as.character(kweight_lst[[i]])
+
+
+lapply(ksmb_lst[[i]],function(x){ 
+                                temp<-get(getSymbols(x,from=start_date))[,1:5];
+                                colnames(temp) <- c('open','high','low','close','volume');
+                                assign(x,temp,envir=tickerData)
+                                }
+)
+
+
+
+ref_xts <- get(ksmb_lst[[i]][1],envir=tickerData)
+
+
+temp_bbl <- BBands(ref_xts[,c("high","low","close")],n=bb_win_szl)
+temp_atrl <- ATR(ref_xts[,c("high","low","close")],n=bb_win_szl)
+
+temp_bbs <- BBands(ref_xts[,c("high","low","close")],n=bb_win_szs)
+temp_atrs <- ATR(ref_xts[,c("high","low","close")],n=bb_win_szs)
+
+
+temp_bbl$atrl <- temp_atrl$trueLow
+temp_bbl$dn <- temp_bbs$dn
+temp_bbl <- temp_bbl[,-4]
+
+
+
+
+ref_prices=do.call(cbind,lapply(ksmb_lst[[i]],function(x) coredata(Cl(get(x,envir=tickerData)[ref_date]))))
+
+prices_run=do.call(cbind,lapply(ksmb_lst[[i]],function(x){print(x); coredata(Cl(get(x,envir = tickerData)))}))
+
+prices_run_idx = sweep(prices_run,2,ref_prices,'/')*100
+
+
+
+colnames(prices_run_idx) <- ksmb_lst[[i]]
+
+prices_run_idx_sort<-prices_run_idx[,(order(colSums(tail(prices_run_idx[,-1])),decreasing = T)+1)[1:3]]
+
+prices_run_idx_sort<- cbind(prices_run_idx[,1],prices_run_idx_sort)
+colnames(prices_run_idx_sort)[1] <- ksmb_lst[[i]][1]
+temp_bbl <- (temp_bbl/ref_prices[1])*100
+
+xts_tmp <- cbind(temp_bbl,prices_run_idx_sort)
+
+df_tmp[[i]] <- data.frame(date=index(xts_tmp),coredata(xts_tmp))
+
+}
+
+
+
+
+maxlen <- max(unlist(lapply(df_tmp,dim)))
+
+tmp <-do.call(cbind,lapply(df_tmp,function(x){fillrow<-maxlen-nrow(x);if(fillrow != 0) x[(nrow(x)+1):(nrow(x)+fillrow),]<-NA;x}))
+
+write_asgs_sheet(tmp,"pead.us","A1")
