97895c4488f625004e9e350b786dc369ae2ba054
coatle0 <coatle0@gmail.com>
2024-07-24 21:35:13 +0900

    update read_html

--- dart_ir2.py
+++ dart_ir2.py
@@ -66,6 +66,7 @@ rtl_df.to_excel(excel_writer=fn,index=False)
 
 
 for i in range (0,test.shape[0]):
+#for i in range (0,5):
     #url = tgt_url.iloc[i]
     test['stockfu'].iloc[i] = mypkg.issf(test['corp_name'].iloc[i])
     
@@ -81,11 +82,11 @@ for i in range (0,test.shape[0]):
 
     if (test['corp_name'].iloc[i] != '라피치') and (test['corp_name'].iloc[i] != '루트락'):
         try:
-            table2 = pd.read_html(doc_url, match = '일시', header=0)
+            table2 = pd.read_html(doc_url, match = '시작일', header=0,encoding='utf8')
             txt_buf=table2[0][0:4].to_string()
             test['flr_nm'].iloc[i] = txt_buf
         except ValueError:
-            table2 = pd.read_html(doc_url, match = '시작일', header=0)
+            table2 = pd.read_html(doc_url, match = '일시', header=0)
             txt_buf=table2[0][0:4].to_string()
             test['flr_nm'].iloc[i] = txt_buf
         except ValueError:
@@ -102,10 +103,5 @@ for i in range (0,test.shape[0]):
     time.sleep(3)
 
 test.to_excel(excel_writer=fn,index=False)
-
-
-
-
-
-#os.system('rscript put_ir_gs.R')
+os.system('rscript put_ir_gs.R')
 
