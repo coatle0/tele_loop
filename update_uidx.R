9225db832bf3b2d8b33720cd5331ec3ae23bb3a6
coatle0 <coatle0@gmail.com>
2024-08-25 14:50:13 +0900

    dbug

--- update_uidx.R
+++ update_uidx.R
@@ -2,7 +2,7 @@ library(telegram.bot)
 bot=Bot(token="5824250303:AAF30nE1zYlP28DzS-Gd69yAegN-LgHU_ag")
 chat_id <- 1278251780
 
-bot_gosu = Bot(token = "7297044765:AAHxCAZ_ETjoG3lyDTkvggM3i4drCL_5Y4g")
+#bot_gosu = Bot(token = "7297044765:AAHxCAZ_ETjoG3lyDTkvggM3i4drCL_5Y4g")
 
 #args=commandArgs(trailingOnly=TRUE)
 msg_text <- 'update uidx'
