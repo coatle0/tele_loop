93cdc8c7d630942b76fd96fe63043c4f79ec001a
coatle0 <coatle0@gmail.com>
2024-08-25 14:42:32 +0900

    merge tele

--- update_uidx.R
+++ update_uidx.R
@@ -1,3 +1,16 @@
+library(telegram.bot)
+bot=Bot(token="5824250303:AAF30nE1zYlP28DzS-Gd69yAegN-LgHU_ag")
+chat_id <- 1278251780
+
+bot_gosu = Bot(token = "7297044765:AAHxCAZ_ETjoG3lyDTkvggM3i4drCL_5Y4g")
+
+#args=commandArgs(trailingOnly=TRUE)
+msg_text <- 'update uidx'
+#msg_text <- args[1]
+print("send message")
+
+bot$sendMessage(chat_id = chat_id, text =msg_text)
+
 setwd('~')
 tickerData <- new.env()
 
